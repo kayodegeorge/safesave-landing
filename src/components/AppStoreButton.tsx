d8a4ea028f9770faba347afa70ba3e28b69ed4d8
Kayode George <68115973+kayodegeorge@users.noreply.github.com>
2023-03-24 09:10:13 +0100

    deleted animation wrapper

--- src/components/AppStoreButton.tsx
+++ src/components/AppStoreButton.tsx
@@ -1,24 +1,18 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { AnimationWrapper } from 'react-hover-animation'
+// import { AnimationWrapper } from 'react-hover-animation'
 
 export default function AppStoreButton() {
   return (
     <div>
       <Link href={'/#'}>
-        <AnimationWrapper
-          animationConfig={{
-            duration: 100,
-          }}
-        >
-          <Image
-            alt='App store image'
-            src={'/assets/appstore-dn.svg'}
-            width={150}
-            height={50}
-          ></Image>
-        </AnimationWrapper>
+        <Image
+          alt='App store image'
+          src={'/assets/appstore-dn.svg'}
+          width={150}
+          height={50}
+        ></Image>
       </Link>
     </div>
   )
--- src/components/GooglePlayButton.tsx
+++ src/components/GooglePlayButton.tsx
@@ -1,24 +1,17 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { AnimationWrapper } from 'react-hover-animation'
 
 export default function GooglePlayButton() {
   return (
     <div>
       <Link href={'/#'}>
-        <AnimationWrapper
-          animationConfig={{
-            duration: 100,
-          }}
-        >
-          <Image
-            alt='App store image'
-            src={'/assets/googleplay-dn.svg'}
-            width={150}
-            height={50}
-          ></Image>
-        </AnimationWrapper>
+        <Image
+          alt='App store image'
+          src={'/assets/googleplay-dn.svg'}
+          width={150}
+          height={50}
+        ></Image>
       </Link>
     </div>
   )
