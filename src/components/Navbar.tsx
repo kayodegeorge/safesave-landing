97a63902f0cc53f30ae55686b112541d7669e6c5
Tobi Samuel <ufomatobi@gmail.com>
2023-03-28 17:49:31 +0100

    Create cookie banner

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,53 +1,53 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { useState } from 'react'
-import { AiOutlineClose } from 'react-icons/ai'
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Navbar() {
-  const [navOpen, setNavOpen] = useState(false)
-  const toggleNav = () => setNavOpen(!navOpen)
+  const [navOpen, setNavOpen] = useState(false);
+  const toggleNav = () => setNavOpen(!navOpen);
 
   return (
     <nav>
-      <div className='mx-auto flex max-w-7xl items-center justify-between px-4 py-4 lg:px-8'>
-        <Link href='/'>
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 lg:px-8">
+        <Link href="/">
           <Image
-            src='/assets/safesave-logo.svg'
+            src="/assets/safesave-logo.svg"
             width={50}
             height={100}
-            alt='Safesave logo'
+            alt="Safesave logo"
           />
         </Link>
 
-        <div className='flex items-center'>
-          <div className='hidden space-x-16 md:block '>
-            <Link className='font-semibold hover:text-safe-light' href='/about'>
+        <div className="flex items-center">
+          <div className="hidden space-x-16 md:block ">
+            <Link className="font-semibold hover:text-safe-light" href="/about">
               About
             </Link>
-            <Link className='font-semibold hover:text-safe-light' href='/about'>
+            <Link className="font-semibold hover:text-safe-light" href="/about">
               Privacy
             </Link>
 
             <a
-              href='#'
-              className='rounded-md bg-safe-blue py-3 px-6 font-medium text-white hover:bg-safe-orange hover:delay-100'
+              href="#"
+              className="rounded-md bg-safe-blue py-3 px-6 font-medium text-white hover:bg-safe-orange hover:delay-100"
             >
               Download app
             </a>
           </div>
 
-          <button type='button' className='md:hidden' onClick={toggleNav}>
+          <button type="button" className="md:hidden" onClick={toggleNav}>
             {/* hamburger Icon */}
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              className='h-5 w-5 text-safe-blue'
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5 text-safe-blue"
             >
               <path
-                fill-rule='evenodd'
-                d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z'
-                clip-rule='evenodd'
+                fillRule="evenodd"
+                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z"
+                clipRule="evenodd"
               />
             </svg>
           </button>
@@ -57,13 +57,13 @@ export default function Navbar() {
       {/* Nav Menu */}
       <div
         className={`fixed top-0 left-0 z-50 mx-auto  h-screen w-full transform bg-white transition-all delay-100 duration-300 ease-in-out ${
-          navOpen ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0'
+          navOpen ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"
         } md:hidden`}
       >
         <button onClick={() => setNavOpen(false)}>
-          <AiOutlineClose className='mt-4 p-3 text-safe-blue' size={50} />
+          <AiOutlineClose className="mt-4 p-3 text-safe-blue" size={50} />
         </button>
       </div>
     </nav>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import { BsArrowLeftCircle, BsArrowRightCircle } from 'react-icons/bs'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 
-import RatingsStar from '@/components/RatingsStar'
-import Navbar from '@/components/Navbar'
-import AppStoreButton from '@/components/AppStoreButton'
-import GooglePlayButton from '@/components/GooglePlayButton'
-import Footer from '@/components/Footer'
-import BenefitsCardSave from '@/components/BenefitsCards/BenefitsCardSave'
-import BenefitsCardStash from '@/components/BenefitsCards/BenefitsCardStash'
-import BenefitsCardInvest from '@/components/BenefitsCards/BenefitsCardInvest'
-import BenefitsCardPayBills from '@/components/BenefitsCards/BenefitsCardPayBills'
-import CarouselCards from '@/components/CarouselCards'
+import RatingsStar from "@/components/RatingsStar";
+import Navbar from "@/components/Navbar";
+import AppStoreButton from "@/components/AppStoreButton";
+import GooglePlayButton from "@/components/GooglePlayButton";
+import Footer from "@/components/Footer";
+import BenefitsCardSave from "@/components/BenefitsCards/BenefitsCardSave";
+import BenefitsCardStash from "@/components/BenefitsCards/BenefitsCardStash";
+import BenefitsCardInvest from "@/components/BenefitsCards/BenefitsCardInvest";
+import BenefitsCardPayBills from "@/components/BenefitsCards/BenefitsCardPayBills";
+import CarouselCards from "@/components/CarouselCards";
+import CookiesModal from "@/components/CookiesModal";
 
 // import { AnimationWrapper } from 'react-hover-animation'
 
@@ -21,13 +22,15 @@ export default function Home() {
     <>
       <Navbar />
 
-      <section className='mx-auto mt-8 max-w-7xl px-4 lg:mt-10 lg:px-8'>
-        <div className='lg:flex'>
+      <CookiesModal />
+
+      <section className="mx-auto mt-8 max-w-7xl px-4 lg:mt-10 lg:px-8">
+        <div className="lg:flex">
           {/* left/top */}
-          <div className='h-full pr-14 lg:w-3/5'>
-            <h1 className='text-5xl lg:text-[64px] lg:leading-[76px]'>
-              <span className='text-safe-orange'>Group Savings,</span> <br />
-              <span className='text-safe-blue'>Credit and Investments.</span>
+          <div className="h-full pr-14 lg:w-3/5">
+            <h1 className="text-5xl lg:text-[64px] lg:leading-[76px]">
+              <span className="text-safe-orange">Group Savings,</span> <br />
+              <span className="text-safe-blue">Credit and Investments.</span>
             </h1>
             {/* <Typed
               className='text-2xl lg:text-[64px] leading-[76px] mb-6 text-safe-blue'
@@ -44,18 +47,18 @@ export default function Home() {
               backSpeed={140}
               loop
             /> */}
-            <p className='mt-4 text-lg font-normal text-safe-light lg:text-xl'>
+            <p className="mt-4 text-lg font-normal text-safe-light lg:text-xl">
               We bring about Group savings, Credit and Investment to help SME &
               Corporate Employees in building their interest.
             </p>
 
             {/* Download buttons */}
-            <div className='mt-4 flex gap-4'>
+            <div className="mt-4 flex gap-4">
               <AppStoreButton />
               <GooglePlayButton />
             </div>
 
-            <div className='mt-4 flex gap-2 text-safe-light'>
+            <div className="mt-4 flex gap-2 text-safe-light">
               <span>Excellent 4.5 out of 5</span>
 
               <RatingsStar />
@@ -63,26 +66,26 @@ export default function Home() {
           </div>
 
           {/* right/bottom */}
-          <div className='h-90 lg:w-2/5 mb-5'>
+          <div className="relative mt-8 hidden h-96 w-full md:block lg:mt-0 lg:w-2/5">
             <Image
-              src={'/assets/heroImg.jpg'}
-              alt='Save woman'
-              width={700}
-              height={300}
-            ></Image>
+              src={"/assets/heroImg.jpg"}
+              alt="Save woman"
+              fill
+              className="object-cover object-center"
+            />
           </div>
         </div>
       </section>
 
-      <section className='mt-8 bg-gray-100'>
-        <div className='mx-auto max-w-7xl px-4 pt-12 pb-28 lg:px-8'>
-          <div className='text-center'>
-            <h2 className='text-4xl font-semibold lg:text-5xl'>
+      <section className="mt-8 bg-gray-100">
+        <div className="mx-auto max-w-7xl px-4 pt-12 pb-28 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-4xl font-semibold lg:text-5xl">
               Save automatically or on your terms, <br />
               set daily, weekly, or monthly savings.
             </h2>
 
-            <div className='mt-4 text-safe-light'>
+            <div className="mt-4 text-safe-light">
               <p>
                 SafeSave leverages our wealth of 3 decades of experience
                 delivering value to our customers,
@@ -94,7 +97,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className='mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4'>
+          <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
             <BenefitsCardSave />
             <BenefitsCardStash />
             <BenefitsCardInvest />
@@ -104,158 +107,158 @@ export default function Home() {
       </section>
 
       <div
-        id='default-carousel'
-        className='relative w-full bg-black'
-        data-carousel='slide'
+        id="default-carousel"
+        className="relative w-full bg-black"
+        data-carousel="slide"
       >
-        <div className='relative h-64 overflow-hidden rounded-lg md:h-96'>
-          <div className='hidden duration-700 ease-in-out' data-carousel-item>
+        <div className="relative h-64 overflow-hidden rounded-lg md:h-96">
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
             <Image
-              src={'/assets/unsplash-new.svg'}
-              alt='Join our tribe'
+              src={"/assets/unsplash-new.svg"}
+              alt="Join our tribe"
               width={800}
               height={300}
             />
           </div>
 
-          <div className='hidden duration-700 ease-in-out' data-carousel-item>
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
             <Image
-              src={'/assets/unsplash-new.svg'}
-              alt='Join our tribe'
+              src={"/assets/unsplash-new.svg"}
+              alt="Join our tribe"
               width={800}
               height={300}
             />
           </div>
-          <div className='hidden duration-700 ease-in-out' data-carousel-item>
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
             <Image
-              src={'/assets/unsplash-new.svg'}
-              alt='Join our tribe'
+              src={"/assets/unsplash-new.svg"}
+              alt="Join our tribe"
               width={800}
               height={300}
             />
           </div>
 
-          <div className='hidden duration-700 ease-in-out' data-carousel-item>
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
             <Image
-              src={'/assets/unsplash-new.svg'}
-              alt='Join our tribe'
+              src={"/assets/unsplash-new.svg"}
+              alt="Join our tribe"
               width={800}
               height={300}
             ></Image>
           </div>
         </div>
         {/* slider indicators */}
-        <div className='absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3'>
+        <div className="absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3">
           <button
-            type='button'
-            className='h-3 w-3 rounded-full'
-            aria-current='true'
-            aria-label='Slide 1'
-            data-carousel-slide-to='0'
+            type="button"
+            className="h-3 w-3 rounded-full"
+            aria-current="true"
+            aria-label="Slide 1"
+            data-carousel-slide-to="0"
           ></button>
           <button
-            type='button'
-            className='h-3 w-3 rounded-full'
-            aria-current='false'
-            aria-label='Slide 2'
-            data-carousel-slide-to='1'
+            type="button"
+            className="h-3 w-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 2"
+            data-carousel-slide-to="1"
           ></button>
           <button
-            type='button'
-            className='h-3 w-3 rounded-full'
-            aria-current='false'
-            aria-label='Slide 3'
-            data-carousel-slide-to='2'
+            type="button"
+            className="h-3 w-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 3"
+            data-carousel-slide-to="2"
           ></button>
           <button
-            type='button'
-            className='h-3 w-3 rounded-full'
-            aria-current='false'
-            aria-label='Slide 4'
-            data-carousel-slide-to='3'
+            type="button"
+            className="h-3 w-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 4"
+            data-carousel-slide-to="3"
           ></button>
         </div>
         {/* <!-- Slider controls --> */}
         <button
-          type='button'
-          className='group absolute top-0 left-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none'
+          type="button"
+          className="group absolute top-0 left-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
           data-carousel-prev
         >
-          <span className='inline-flex h-8 w-8 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70 sm:h-10 sm:w-10'>
+          <span className="inline-flex h-8 w-8 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70 sm:h-10 sm:w-10">
             <svg
-              aria-hidden='true'
-              className='h-5 w-5 text-white dark:text-gray-800 sm:h-6 sm:w-6'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'
+              aria-hidden="true"
+              className="h-5 w-5 text-white dark:text-gray-800 sm:h-6 sm:w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M15 19l-7-7 7-7'
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 19l-7-7 7-7"
               ></path>
             </svg>
-            <span className='sr-only'>Previous</span>
+            <span className="sr-only">Previous</span>
           </span>
         </button>
         <button
-          type='button'
-          className='group absolute top-0 right-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none'
+          type="button"
+          className="group absolute top-0 right-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
           data-carousel-next
         >
-          <span className='inline-flex h-8 w-8 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70 sm:h-10 sm:w-10'>
+          <span className="inline-flex h-8 w-8 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70 sm:h-10 sm:w-10">
             <svg
-              aria-hidden='true'
-              className='h-5 w-5 text-white dark:text-gray-800 sm:h-6 sm:w-6'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'
+              aria-hidden="true"
+              className="h-5 w-5 text-white dark:text-gray-800 sm:h-6 sm:w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M9 5l7 7-7 7'
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 5l7 7-7 7"
               ></path>
             </svg>
-            <span className='sr-only'>Next</span>
+            <span className="sr-only">Next</span>
           </span>
         </button>
       </div>
 
-      <section className='mx-auto max-w-7xl px-4 pt-16 pb-16 lg:px-8 lg:pb-60'>
-        <h3 className='text-center text-4xl font-bold'>
+      <section className="mx-auto max-w-7xl px-4 pt-16 pb-16 lg:px-8 lg:pb-60">
+        <h3 className="text-center text-4xl font-bold">
           What our customers are saying
         </h3>
 
-        <div className='mt-12 flex items-center gap-5'>
-          <button type='button' className='hidden'>
+        <div className="mt-12 flex items-center gap-5">
+          <button type="button" className="hidden">
             <BsArrowLeftCircle size={30} />
           </button>
 
-          <div className='grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16'>
-            <div className='min-w-[200px] rounded-lg bg-white pl-[72px] pb-16 pr-[88px] pt-10 shadow-md'>
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+            <div className="min-w-[200px] rounded-lg bg-white pl-[72px] pb-16 pr-[88px] pt-10 shadow-md">
               <div>
                 <RatingsStar />
               </div>
 
-              <div className='mt-6'>
-                <span className='font-semibold'>
+              <div className="mt-6">
+                <span className="font-semibold">
                   SafeSave allowed me to buy my dream house
                 </span>
 
-                <p className='mt-2 text-safe-light'>
+                <p className="mt-2 text-safe-light">
                   I was bad with my money management for a long time until i
                   opened a safesave account, my life has not remained the same
                 </p>
 
-                <div className='mt-6 w-px bg-[#DFE4FF]'></div>
+                <div className="mt-6 w-px bg-[#DFE4FF]"></div>
 
-                <div className='flex items-center gap-4'>
-                  <div className='h-11 w-11 rounded-full border-2 border-[#2948FF] bg-gray-400'></div>
+                <div className="flex items-center gap-4">
+                  <div className="h-11 w-11 rounded-full border-2 border-[#2948FF] bg-gray-400"></div>
 
                   <span>
                     Chinedu <br /> Ndunaka
@@ -264,25 +267,25 @@ export default function Home() {
               </div>
             </div>
 
-            <div className='min-w-[200px] rounded-lg bg-white pl-[72px] pb-16 pr-[88px] pt-10 shadow-md'>
+            <div className="min-w-[200px] rounded-lg bg-white pl-[72px] pb-16 pr-[88px] pt-10 shadow-md">
               <RatingsStar />
 
-              <div className='mt-6'>
-                <span className='font-semibold'>
+              <div className="mt-6">
+                <span className="font-semibold">
                   I finally started my business thanks to SafeSave
                 </span>
 
-                <p className='mt-2 text-safe-light'>
+                <p className="mt-2 text-safe-light">
                   I have not been able to kickstart my business because i have
                   not seen where i can save my money, i stumbled upon safesave
                   and after a year i was able to start with their wonderful
                   interest rates.
                 </p>
 
-                <div className='mt-6 w-px bg-[#DFE4FF]'></div>
+                <div className="mt-6 w-px bg-[#DFE4FF]"></div>
 
-                <div className='flex items-center gap-4'>
-                  <div className='h-11 w-11 rounded-full border-2 border-[#2948FF] bg-gray-400'></div>
+                <div className="flex items-center gap-4">
+                  <div className="h-11 w-11 rounded-full border-2 border-[#2948FF] bg-gray-400"></div>
 
                   <span>
                     Ajanaku <br /> Robiat
@@ -292,34 +295,34 @@ export default function Home() {
             </div>
           </div>
 
-          <button type='button' className='hidden'>
+          <button type="button" className="hidden">
             <BsArrowRightCircle size={30} />
           </button>
         </div>
       </section>
 
-      <section className='flex h-96 justify-center bg-gradient-to-b from-[#3770C1] to-[#092C5F]'>
-        <div className='relative hidden w-96 md:block'>
-          <div className='absolute left-0 -top-28 h-96 w-96'>
+      <section className="flex h-96 justify-center bg-gradient-to-b from-[#3770C1] to-[#092C5F]">
+        <div className="relative hidden w-96 md:block">
+          <div className="absolute left-0 -top-28 h-96 w-96">
             <Image
-              src={'/assets/unsplash-new.svg'}
-              alt='Join our tribe'
+              src={"/assets/unsplash-new.svg"}
+              alt="Join our tribe"
               fill
-              className='object-cover object-top'
+              className="object-cover object-top"
             />
           </div>
         </div>
 
-        <div className='mt-14 pl-4 md:pl-28'>
-          <h2 className='text-5xl text-white'>Join the tribe.</h2>
+        <div className="mt-14 pl-4 md:pl-28">
+          <h2 className="text-5xl text-white">Join the tribe.</h2>
 
-          <p className='mt-4 text-white'>
+          <p className="mt-4 text-white">
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. <br /> Velit officia consequat duis enim velit mollit.
             Exercitation veniam consequat sunt <br /> nostrud amet.
           </p>
 
-          <div className='mt-8 flex gap-4'>
+          <div className="mt-8 flex gap-4">
             <AppStoreButton />
             <GooglePlayButton />
           </div>
@@ -328,5 +331,5 @@ export default function Home() {
 
       <Footer />
     </>
-  )
+  );
 }
